fa8d98eb6924d01485462ef3237767d4672b09d3
Mohamed Shawky Bayoumi <mohamedshawkybayoumi@gmail.com>
2018-05-31 02:32:41 +0200

    first commit

---
+++ lesson-1-async-w-xhr/app.js
@@ -0,0 +1,24 @@
+(function () {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+
+        const searchedForText = 'hippos';
+        const unsplashRequest = new XMLHttpRequest();
+        
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.onload = addImage;
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID <your-client-id>');
+        unsplashRequest.send();
+        
+        function addImage(){
+        }
+        
+    });
+})();
